ec3b77e5cfc20e8bfe8950d1c0a4ceba4ac00960
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2024-01-25 17:37:47 +0330

    feat: :sparkles: footer responsive

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,19 +1,23 @@
 import Container from '../container';
 import Icon from '../icon';
 import SvgLogoVertical from '~/assets/logo-vertical.svg';
+import SvgLogo from '~/assets/logo.svg';
 
 export type FooterProps = {};
 
 const Footer = ({}: FooterProps) => {
   return (
     <Container className="mb-12 mt-16">
-      <div className="flex items-center justify-between rounded-2xl border border-[#191919] px-6 py-2.5">
-        <p className="my-1.5 text-base font-500 text-white">
+      <div className="flex items-center justify-between rounded-2xl border border-[#191919] px-6 py-2.5 sm:py-4">
+        <p className="my-1.5 text-base font-500 text-white sm:text-xs">
           All right reserved to White Deer
         </p>
 
-        <Icon>
+        <Icon className="block sm:hidden">
           <SvgLogoVertical className="h-12" />
+        </Icon>
+        <Icon className="hidden sm:block">
+          <SvgLogo className="origin-right scale-75" />
         </Icon>
       </div>
     </Container>
