d55c6fb6cdeedaeb6ced07c9888f6f6a73d2fe91
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2024-01-25 14:08:13 +0330

    fix: :bug: labeled title

--- src/components/labeled-title/index.tsx
+++ src/components/labeled-title/index.tsx
@@ -6,9 +6,9 @@ export type LabeledTitleProps = {
 const LabeledTitle = ({ label, title }: LabeledTitleProps) => {
   return (
     <div className="flex justify-center gap-2">
-      <h1 className="text-[#989898]">{label}</h1>
+      <h1 className="text-[#989898]">{title}</h1>
       <span className="flex h-[26px] items-center justify-center rounded-full border border-[#191919] px-4 py-2 text-[8px] text-white">
-        {title}
+        {label}
       </span>
     </div>
   );
