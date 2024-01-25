ecc5e467c57ef505df086566657b227c228b6803
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2024-01-25 17:43:39 +0330

    feat: :sparkles: subscribe responsive

--- src/components/views/subscribe/index.tsx
+++ src/components/views/subscribe/index.tsx
@@ -28,11 +28,11 @@ const Subscribe = ({}: SubscribeProps) => {
   return (
     <Container>
       <div className="mt-44 flex flex-col items-center text-center">
-        <h1 className="text-5xl font-400 -tracking-[2px] text-white">
+        <h1 className="text-5xl font-400 -tracking-[2px] text-white sm:text-2xl">
           Kn
           <span
             ref={oRef}
-            className="mx-1 inline-block h-[26px] w-[26px] rounded-full border-[3px] border-white"
+            className="mx-1 inline-block h-[26px] w-[26px] rounded-full border-[3px] border-white sm:h-3 sm:w-3 sm:border-[2px]"
           />
           w more about
           <br />
@@ -41,12 +41,12 @@ const Subscribe = ({}: SubscribeProps) => {
 
         <form
           onSubmit={(e) => e.preventDefault()}
-          className="mt-8 flex w-full max-w-lg overflow-hidden rounded-2xl bg-[#0D0D0D] p-1 focus-within:bg-[#111111]"
+          className="mt-8 flex w-full max-w-lg overflow-hidden rounded-2xl bg-[#0D0D0D] p-1 focus-within:bg-[#111111] sm:mt-6 sm:flex-col sm:gap-2 sm:rounded-none sm:bg-transparent sm:p-0 sm:focus-within:bg-transparent"
         >
           <input
             type="text"
             placeholder="Enter your email"
-            className="h-full grow bg-transparent py-5 pl-6 font-500 text-white placeholder:text-[#5B5B5B] focus-visible:outline-0"
+            className="h-full grow bg-transparent py-5 pl-6 font-500 text-white placeholder:text-[#5B5B5B] focus-visible:outline-0 sm:rounded-2xl sm:bg-[#0D0D0D] sm:focus-within:bg-[#111111]"
           />
           <Button size="lg">Subscribe</Button>
         </form>
