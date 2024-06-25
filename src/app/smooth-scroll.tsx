a0f59e87b68a234e04b645ee0a9d8faa8ede41cb
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2024-06-25 17:17:20 +0330

    feat: add more logic to prevent smooth scroll on mobile

--- src/app/smooth-scroll.tsx
+++ src/app/smooth-scroll.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Lenis as ReactLenis, useLenis } from '@studio-freight/react-lenis';
-import { useEffect } from 'react';
-import { useMenu } from '~/hooks/ui';
+import { useEffect, useMemo } from 'react';
+import { useMenu } from '~/hooks/ui/use-menu';
+import { isIos, isMobile } from '~/utils';
+
 export type SmoothScrollProps = React.PropsWithChildren;
 
 const SmoothScroll = ({ children }: SmoothScrollProps) => {
@@ -12,6 +14,10 @@ const SmoothScroll = ({ children }: SmoothScrollProps) => {
   useEffect(() => {
     if (lenis) lenis.isLocked = state === 'opened';
   }, [lenis, state]);
+
+  const isMobileDevise = useMemo(() => isMobile() || isIos(), []);
+
+  if (isMobileDevise) return children;
 
   return <ReactLenis root>{children}</ReactLenis>;
 };
