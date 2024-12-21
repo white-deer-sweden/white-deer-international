a5e4c3d63d22e4614a2bde5037c980561a261c60
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2024-12-21 19:36:48 +0330

    fix: texts

--- src/components/labeled-title/index.tsx
+++ src/components/labeled-title/index.tsx
@@ -7,7 +7,7 @@ const LabeledTitle = ({ label, title }: LabeledTitleProps) => {
   return (
     <div className="flex justify-center gap-2 sm:items-center">
       <h1 className="text-[#989898] sm:text-xs">{title}</h1>
-      <span className="flex h-[26px] items-center justify-center rounded-full border border-[#191919] px-4 py-2 text-[8px] text-white">
+      <span className="flex h-[26px] items-center justify-center rounded-full border border-[#191919] px-4 py-2 text-[10px] text-white">
         {label}
       </span>
     </div>
--- src/components/views/sustainability/index.tsx
+++ src/components/views/sustainability/index.tsx
@@ -19,17 +19,15 @@ const Sustainability = ({}: SustainabilityProps) => {
     <Container className="mt-36" id={SECTIONS.RECYCLING_ESSENCE}>
       <LabeledTitle title="Scented Sustainability" label="Recycling Essence" />
 
-      <h1 className="mt-3 text-center text-2xl text-[38px] font-400 leading-tight -tracking-[2px] text-white sm:text-2xl sm:leading-tight">
-        Fragrance Harmony: The
-        <br className="hidden sm:block" />
+      <h1 className="mt-3 text-center text-2xl text-[38px] font-400 leading-tight -tracking-[1px] text-white sm:text-2xl sm:leading-tight">
+        Fragrance Harmony: The <br className="hidden sm:block" />
         Essence of <br className="sm:hidden" />
-        Sustainable Perfumes
-        <br className="hidden sm:block" />
+        Sustainable Perfumes <br className="hidden sm:block" />
         and Recycling
       </h1>
 
       <div className="mt-[52px] grid min-h-[410px] grid-cols-12 grid-rows-3 gap-2 sm:mt-6 sm:h-auto sm:grid-cols-5 sm:grid-rows-[repeat(4,auto)] sm:gap-6">
-        <div className="col-span-5 row-span-full flex h-full flex-col rounded-4xl border border-[#191919] p-6 sm:col-span-full sm:row-start-2 sm:row-end-3 sm:rounded-3xl lg:col-span-6">
+        <div className="col-span-5 row-span-full flex h-full flex-col rounded-4xl border border-[#191919] p-6 lg:col-span-6 sm:col-span-full sm:row-start-2 sm:row-end-3 sm:rounded-3xl">
           <div className="flex gap-2">
             {['Recycle', 'Recycled Glass'].map((i) => (
               <Label key={i}>{i}</Label>
@@ -48,7 +46,7 @@ const Sustainability = ({}: SustainabilityProps) => {
           </div>
         </div>
 
-        <div className="col-span-4 col-start-6 row-span-2 h-full rounded-4xl bg-[#090909] p-6 sm:col-span-full sm:row-start-1 sm:row-end-2 sm:h-auto sm:rounded-3xl sm:p-2 lg:col-start-7 lg:col-span-6">
+        <div className="col-span-4 col-start-6 row-span-2 h-full rounded-4xl bg-[#090909] p-6 lg:col-span-6 lg:col-start-7 sm:col-span-full sm:row-start-1 sm:row-end-2 sm:h-auto sm:rounded-3xl sm:p-2">
           <div className="space-y-4 sm:space-y-2">
             {[
               {
@@ -69,7 +67,7 @@ const Sustainability = ({}: SustainabilityProps) => {
           </div>
         </div>
 
-        <div className="relative col-span-4 col-start-6 row-span-1 h-full overflow-hidden rounded-4xl border border-[#191919] p-6 sm:col-span-full sm:row-start-4 sm:row-end-5 sm:rounded-3xl sm:p-4 lg:col-start-7 lg:col-span-6">
+        <div className="relative col-span-4 col-start-6 row-span-1 h-full overflow-hidden rounded-4xl border border-[#191919] p-6 lg:col-span-6 lg:col-start-7 sm:col-span-full sm:row-start-4 sm:row-end-5 sm:rounded-3xl sm:p-4">
           <h6 className="text-xl font-500 text-white sm:text-lg">
             Radiant Choices: Candle Selection
           </h6>
@@ -94,7 +92,7 @@ const Sustainability = ({}: SustainabilityProps) => {
           </Icon>
         </div>
 
-        <div className="col-span-3 col-start-10 row-span-full h-full rounded-4xl bg-[#090909] p-8 text-center lg:hidden sm:col-span-full sm:row-start-3 sm:row-end-4 sm:rounded-3xl sm:p-4 sm:block">
+        <div className="col-span-3 col-start-10 row-span-full h-full rounded-4xl bg-[#090909] p-8 text-center lg:hidden sm:col-span-full sm:row-start-3 sm:row-end-4 sm:block sm:rounded-3xl sm:p-4">
           <div>
             <Image
               src={ImgAnons}
